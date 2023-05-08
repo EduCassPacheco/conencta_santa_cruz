9d2a4a265465395cf4b4794c39ca035119765a01
EduCassPacheco <edu.celular2019@gmail.com>
2023-05-08 12:44:16 -0300

    Adicionado estrutura do getX

---
+++ lib/app/routes/app_pages.dart
@@ -0,0 +1,14 @@
+import 'package:conecta_santa_cruz/app/modules/home_binding.dart';
+import 'package:conecta_santa_cruz/app/modules/home_page.dart';
+import 'package:conecta_santa_cruz/app/routes/app_routes.dart';
+import 'package:get/get.dart';
+
+class AppPages {
+  static final List<GetPage> pages = [
+    GetPage(
+      name: AppRoutes.home,
+      page: () => HomePage(),
+      binding: HomeBinding(),
+    ),
+  ];
+}
